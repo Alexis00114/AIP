de6042712efb647d76b0239e25742fdeeda37c3a
uwanyuzedeborah <Uwanyuzedeborah1@gamil.com>
2026-04-28 06:32:00 -0700

    firsy commit

---
+++ louange/vueapp/Alexis/src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import Studentlist from './components/Studentlist.vue';
+export default{
+components: {
+  Studentlist,
+}
+}
+</script>
+<template>
+  <Studentlist/>
+</template>
---
+++ louange/vueapp/Alexis/src/components/Studentlist.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="table-container">
+        <h2>List of students</h2>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Level</th>
+                <th>Trade</th>
+                </tr>
+            <tr v-for="data in student" :key="data.id">
+            <td>{{data.id}}</td>
+            <td>{{data.name}}</td>
+            <td>{{data.level}}</td>
+            <td>{{data.trade}}</td>
+            
+
+            </tr>
+        </table>
+    </div>
+    <div><p>We have many students but those aaaare Examples o</p>
+        <h1>REGTRATION FORM</h1>
+        <P>Login to your account</P>
+    </div>
+
+</template>
+<script>
+export default {
+    data(){
+        return{
+            student: [
+                {id:1,name:"ALEXIS",level:4,trade:"SWD"},
+                {id:2,name:"LUOANGE",level:3,trade:"SWD"},
+                {id:3,name:"MANZI",level:5,trade:"SWD"},
+                {id:3,name:"MANZI",level:5,trade:"SWD"},
+                {id:3,name:"MANZI",level:5,trade:"SWD"},
+                {id:3,name:"MANZI",level:5,trade:"SWD"},
+                {id:4,name:"DOXA",level:3,trade:"SWD"}
+            ]
+
+        }
+    }
+}
+</script>
